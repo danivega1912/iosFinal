e278023af810ceb5a199eb9a44dbcef12469d51f
Daniel Vega <danivega@gmail.com>
2018-07-01 12:13:29 -0300

    Agrego modelos: Hamburguesa y Pizza

--- FinalProject/API/ApiSimulator.swift
+++ FinalProject/API/ApiSimulator.swift
@@ -24,13 +24,13 @@ class ApiSimulator {
         let dish0:Dish = Dish.init(name: "Torta de Chocolate", model3dName: "Cake.dae", price: 125.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.000013, model3dScaleY: 0.000013, model3dScaleZ: 0.000013)
         dishes.append(dish0)
         
-        let dish1:Dish = Dish.init(name: "Chocolate Cake 1", model3dName: "Cake.dae", price: 126.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.000013, model3dScaleY: 0.000013, model3dScaleZ: 0.000013)
+        let dish1:Dish = Dish.init(name: "Pizza Mediterranea", model3dName: "pizza.dae", price: 126.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.009, model3dScaleY: 0.009, model3dScaleZ: 0.009)
         dishes.append(dish1)
 
-        let dish2:Dish = Dish.init(name: "Chocolate Cake 2", model3dName: "Cake.dae", price: 127.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.000013, model3dScaleY: 0.000013, model3dScaleZ: 0.000013)
+        let dish2:Dish = Dish.init(name: "Torta 2", model3dName: "Cake.dae", price: 127.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.000013, model3dScaleY: 0.000013, model3dScaleZ: 0.000013)
         dishes.append(dish2)
 
-        let dish3:Dish = Dish.init(name: "Chocolate Cake 3", model3dName: "Cake.dae", price: 128.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.000013, model3dScaleY: 0.000013, model3dScaleZ: 0.000013)
+        let dish3:Dish = Dish.init(name: "Hamburguesa c/Queso", model3dName: "burger.dae", price: 250.00, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: -1.6, model3dScaleX: 0.0005, model3dScaleY: 0.0005, model3dScaleZ: 0.0005)
         dishes.append(dish3)
         
         let dish4:Dish = Dish.init(name: "Banana", model3dName: "Banana.dae", price: 15.50, model3dRotationX: 1, model3dRotationY: 0, model3dRotationZ: 0, model3dRotationRad: 0, model3dScaleX: 0.015, model3dScaleY: 0.015, model3dScaleZ: 0.015)
@@ -46,13 +46,13 @@ class ApiSimulator {
             dishDetail = "Sencillo bizcocho de chocolate bañado en una deliciosa cobertura de crema de chocolate... chocolate más chocolate, acierto seguro..."
             break
         case 1:
-            dishDetail = "Detail 1"
+            dishDetail = "Elaborada con trozos de calabacín, pimiento rojo y cebolla asados y sabrosos dados de queso de cabra. ¡Disfruta de nuestra pizza más fresca y natural!"
             break
         case 2:
             dishDetail = "Detail 2"
             break
         case 3:
-            dishDetail = "Detail 3"
+            dishDetail = "Deliciosa carne a la parrilla cubierta con una rebanada de queso amarillo derretido, pepinillos frescos, mostaza y salsa de tomate, sobre un pan suave con ajonjolí."
             break
         default:
             //oopss!
